28527437516be49f6f9eb9a13ef0d7af8691a65d
ichikawa <baseak0527@gmail.com>
2026-08-06 02:06:52 +0900

    詳細ページを作成し個別ページの内容を表示

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,44 @@
-'use cache';
+import Link from 'next/link';
+import { client } from '../../libs/client'
 
-import Image from 'next/image'
+export default async function page({ params }) {
+  const { id } = await params;
 
-export default async function page() {
+  async function getBlogDetail() {
+    const data = await client.get({
+      endpoint: 'blogs',
+      contentId: id,
+    });
 
+    return data;
+  }
+
+  const contents = await getBlogDetail()
+
+  return (
+    <>
+      <div>
+        <p>テスト：{contents.id}</p>
+        <p>タイトル：{contents.title}</p>
+        <p>公開日：{contents.createdAt}</p>
+        <div>
+          <p>カテゴリ</p>
+          {contents.category.length != 0 ?
+            <ul>
+              {contents.category.map((cat) => {
+                return (
+                  <li key={cat.id}>
+                    <p>{cat.title}</p>
+                  </li>
+                )
+              })}
+            </ul>
+            : ""}
+        </div>
+        <div>
+          <Link href="/">戻る</Link>
+        </div>
+      </div>
+    </>
+  )
 } 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-'use cache';
-
+import Link from 'next/link';
 import { client } from '../libs/client'
 import Image from 'next/image'
 
@@ -32,20 +31,18 @@ export default async function Home() {
 
   const dataContents = await getBlogPosts();
 
-
-
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>
           {dataContents.map((item) => {
             return (
-              <div key={item.id}>
+              <Link href={`/${item.id}/`} key={item.id}>
                 {item.thumbnail ?
                   <Image src={item.thumbnail.url} width={item.thumbnail.width} height={item.thumbnail.width} alt=""></Image>
                   : <Image src="/images/noimage.jpg" width="960" height="540" alt=""></Image>}
                 <p>{item.title}</p>
-              </div>
+              </Link>
             )
           })}
         </div>
